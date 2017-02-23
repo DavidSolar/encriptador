95815a62d0683df9039c3ae9ff7836a2e76869d4
dnajerao <dano080695@gmail.com>
2017-02-23 17:51:26 -0600

    Aplica NAND y separa en octetos

--- src/Metodos/OpDesencriptado.java
+++ src/Metodos/OpDesencriptado.java
@@ -5,20 +5,15 @@
  */
 package Metodos;
 
-import Metodos.Operaciones;
-import static Metodos.Operaciones.aplicandoAndConG;
-import static Metodos.Operaciones.ascii;
-import static Metodos.Operaciones.asciiFinal;
-import static Metodos.Operaciones.binarios;
-import static Metodos.Operaciones.tablaConRecorrido;
 import java.util.ArrayList;
 
 /**
  * Pasos para el desencriptado 1. Obtener cadena desde la GUI. 2. Localizar cada
  * caracter en el diccionario y obtener su valor en ascii. 3. Convertir el valor
  * de ascii en binario y completar binario a 8 bits 4. Aplicar desplazamiento a
- * la derecha 2 bits 5. Convertir binario en ASCII 6. Localizar ASCII en el
- * diccionario y obtener el caracter 7. Imprimir un String en la caja de texto
+ * la derecha 2 bits X: Aplicar NAND 5. Convertir binario en ASCII 6. Localizar
+ * ASCII en el diccionario y obtener el caracter 7. Imprimir un String en la
+ * caja de texto
  *
  * @author Diego Najera
  * @return String texto desencriptado
@@ -29,10 +24,10 @@ public class OpDesencriptado {
     public static ArrayList array_binarios = new ArrayList<>();
     public static ArrayList array_and = new ArrayList<>();
     public static ArrayList tablaConRecorrido = new ArrayList<>();
-    public static ArrayList asciiFinal = new ArrayList<>();
+    public static ArrayList array_asciiFinal = new ArrayList<>();
 
     public static void main(String[] args) {
-        desencriptar("HOLA MUNDO");
+        desencriptar("♥▲↕♠♥▬▬→↕▲");
         limpiar();
     }
 
@@ -53,6 +48,10 @@ public class OpDesencriptado {
             array_binarios.add(binario_completo);
             binarios += binario_completo;
         }
+
+        binarios = desplazarDerecha(binarios);
+        binarios = aplicarNAND("01100111", binarios);
+        separarAASCII(binarios);
         limpiar();
 
         return mensaje;
@@ -89,12 +88,61 @@ public class OpDesencriptado {
         return binario_completo;
     }
 
+    public static String desplazarDerecha(String cadena_binarios) {
+        String nueva_cadena = "";
+        nueva_cadena += cadena_binarios.charAt(cadena_binarios.length() - 2);
+        nueva_cadena += cadena_binarios.charAt(cadena_binarios.length() - 1);
+
+        for (int i = 0; i < cadena_binarios.length() - 2; i++) {
+            nueva_cadena += cadena_binarios.charAt(i);
+        }
+
+        return cadena_binarios;
+
+    }
+
+    public static String aplicarNAND(String g, String binarios) {
+        int total_g = binarios.length() / g.length();
+        String cadena_g = "";
+        String cadenaNAND = "";
+        for (int i = 0; i < total_g; i++) {
+            cadena_g += g;
+        }
+
+        /* Aplicaicion NAND */
+        for (int i = 0; i < binarios.length(); i++) {
+            if ((binarios.charAt(i) == '0') && (cadena_g.charAt(i) == '0')) {
+                cadenaNAND += "1";
+            } else {
+                cadenaNAND += "0";
+            }
+        }
+
+        return cadenaNAND;
+    }
+
+    public static String separarAASCII(String cadena_binarios) {
+        System.out.println(cadena_binarios);
+
+        for (int i = 0; i < cadena_binarios.length() / 8; i++) {
+            int limite = (i + 1) * 8;
+            int index = limite - 8;
+            String respuesta = "";
+            for (int j = index; j < limite; j++) {
+                respuesta += cadena_binarios.charAt(j);
+            }
+            System.out.println(respuesta);
+        }
+
+        return "";
+    }
+
     public static void limpiar() {
         array_ascii.clear();
         array_binarios.clear();
         array_and.clear();
         tablaConRecorrido.clear();
-        asciiFinal.clear();
+        array_asciiFinal.clear();
     }
 
 }
